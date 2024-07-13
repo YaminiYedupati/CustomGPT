955157379ba4e3970d4b96678562b543585a0a49
YaminiYedupati <147988230+YaminiYedupati@users.noreply.github.com>
2024-07-13 14:03:56 +0530

    Fine tune with prompt engineering and instruction fine tuning

--- ExtractWebData.py
+++ ExtractWebData.py
@@ -30,8 +30,36 @@ chain = ConversationalRetrievalChain.from_llm(llm,
                                               vectorstore.as_retriever(),
                                               return_source_documents=True)
 
+prompt_1 = "What is SonarQube?"
+completion_1 = "SonarQube is an open-source platform developed by SonarSource for continuous inspection of code quality to perform automatic reviews with static analysis of code to detect bugs, code smells, and security vulnerabilities in over 20 programming languages. It provides reports on code coverage, code duplication, complexity, potential bugs, and more, helping development teams to maintain code quality throughout the development and maintenance phases of their projects."
+prompt_2 = "How can I integrate SonarQube with my application? Provide a very concise answer"
+completion_2 = """Integrating SonarQube with your application typically involves these steps:
+
+        Install SonarQube: Set up SonarQube on a server or cloud instance.
+
+        Configure Your Build: Integrate SonarQube into your build process (e.g., Maven, Gradle, Jenkins) using plugins or build scripts.
+
+        Run Analysis: Execute SonarQube analysis during your build to generate code quality reports.
+
+        View Results: Access and interpret the reports in the SonarQube dashboard to improve code quality.
+
+        Each step may require specific configuration based on your development environment and tools."""
+
+#implement PEFT
+
 with st.form('my_form'):
     text = st.text_area('Enter text:', 'What is SonarQube?')
     submitted = st.form_submit_button('Submit')
     if submitted:
-        st.info(chain({"question": text, "chat_history": []})['answer'])
+        # few shot prompt engineering and instruction fine tuning
+        prompt = f""" Provide an elaborate answer to the questions. Given are some examples. If this is the question: {prompt_1}, then the answer will be: {completion_1}.
+        
+        If this is the question: {prompt_2},
+        
+        then the answer will be: {completion_2}.
+                
+        If this is the question: {text}?, then the answer will be: """
+
+        #TO-DO: add some question answer examples and calculate rouge, bert and bleu score if applicable.
+
+        st.info(chain({"question": prompt, "chat_history": []})['answer'])
